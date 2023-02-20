1978840c820b5d76d9cbe6d49326f20b48d4e390
rongjie.song <rongjie.song@1step.ai>
2023-02-20 16:03:44 +0800

    feat: 配置数据持久化

--- src/stores/user.js
+++ src/stores/user.js
@@ -3,7 +3,8 @@ import { defineStore } from 'pinia'
 // 你可以对 `defineStore()` 的返回值进行任意命名，但最好使用 store 的名字，同时以 `use` 开头且以 `Store` 结尾。(比如 `useUserStore`，`useCartStore`，`useProductStore`)
 // 第一个参数是你的应用中 Store 的唯一 ID。
 const useUserStore = defineStore('userInfo', {
-  state: () => ({ count: 10 }),
+  persist: true, // 数据持久化
+  state: () => ({ count: 15 }),
   getters: {
     double: (state) => state.count * 2,
   },
