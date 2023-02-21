89acc9eb39555d0fa3932f6f4e02cdc0066263a2
rongjie.song <rongjie.song@1step.ai>
2023-02-21 14:59:09 +0800

    feat: 禁用打包文件分析插件

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { visualizer } from 'rollup-plugin-visualizer'
+// 打包分析插件
+// import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,11 +19,11 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-    visualizer({
-      emitFile: false,
-      file: 'stats.html', // 分析图生成的文件名
-      open: false, // 如果存在本地服务端口，将在打包后自动展示
-    }),
+    // visualizer({
+    //   emitFile: false,
+    //   file: 'stats.html', // 分析图生成的文件名
+    //   open: false, // 如果存在本地服务端口，将在打包后自动展示
+    // }),
   ],
   resolve: {
     alias: {
