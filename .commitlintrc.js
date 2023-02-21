9626fbfee9691386e471cba15cfac3e9ceed7484
rongjie.song <rongjie.song@1step.ai>
2023-02-21 14:24:01 +0800

    feat: 完善pre-commit(eslint只修复暂存区文件，prettier格式化所有文件)

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  plugins: [
+    {
+      rules: {
+        "commit-rule": ({ raw }) => {
+          return [
+            /^\[(feat|fix|docs|style|build|refactor|revert|test|perf|chore)].+/g.test(raw),
+            `commit备注信息格式错误，格式为 <[type]: 修改内容>，type支持${types.join(",")}`,
+          ];
+        },
+      },
+    },
+  ],
 }
